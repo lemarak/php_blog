fc552a980b78d6db6998fc33d78eb7fa59c0782b
Stéphane Dauguet <sdauguet@gmail.com>
2021-09-29 15:27:34 +0200

    add filter article in index.php

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 $filename = __DIR__ . "/data/articles.json";
 $articles = [];
 $categories = [];
+$selectedCat = '';
+
+$_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$selectedCat = $_GET['cat'] ?? '';
+
 
 if (file_exists($filename)) {
     $articles = json_decode(file_get_contents($filename), true) ?? [];
@@ -55,22 +60,45 @@ if (file_exists($filename)) {
         require_once("./includes/header.php");
         ?>
         <div class="content">
-            <div class="category-container">
-                <?php foreach ($categories as $category => $nbInCat) : ?>
-                    <h2><?= "$category ($nbInCat)" ?></h2>
-                    <div class="articles-container">
-                        <?php foreach ($articlePerCategories[$category] as $article) : ?>
-                            <div class="article block">
-                                <div class="overflow">
-                                    <div class="img-container" style="background-image:url(<?= $article['image'] ?>)"></div>
-                                </div>
-                                <h3><?= $article['title'] ?></h3>
+            <div class="newsfeed-container">
+                <ul class="category-container">
+                    <li class="<?= $selectedCat ? '' : 'cat-active' ?>"><a href="./"> Tous les articles <span class="small">(<?= count($articles) ?>)</span></a></li>
+                    <?php foreach ($categories as $category => $nbInCat) : ?>
+
+                        <li class="<?= $selectedCat === $category ? 'cat-active' : '' ?>"><a href="./?cat=<?= $category ?>"><?= $category ?> <span class="small">(<?= $nbInCat ?>)</span>
+                            </a></li>
+                    <?php endforeach ?>
+                </ul>
+                <div class="newsfeed-content">
+                    <?php if (!$selectedCat) : ?>
+                        <?php foreach ($categories as $category => $nbInCat) : ?>
+                            <h2><?= $category  ?></h2>
+                            <div class="articles-container">
+                                <?php foreach ($articlePerCategories[$category] as $article) : ?>
+                                    <div class="article block">
+                                        <div class="overflow">
+                                            <div class="img-container" style="background-image:url(<?= $article['image'] ?>)"></div>
+                                        </div>
+                                        <h3><?= $article['title'] ?></h3>
+                                    </div>
+                                <?php endforeach ?>
                             </div>
                         <?php endforeach ?>
-                    </div>
-                <?php endforeach ?>
+                    <?php else : ?>
+                        <h2><?= $selectedCat ?></h2>
+                        <div class="articles-container">
+                            <?php foreach ($articlePerCategories[$selectedCat] as $article) : ?>
+                                <div class="article block">
+                                    <div class="overflow">
+                                        <div class="img-container" style="background-image:url(<?= $article['image'] ?>)"></div>
+                                    </div>
+                                    <h3><?= $article['title'] ?></h3>
+                                </div>
+                            <?php endforeach ?>
+                        </div>
+                    <?php endif ?>
+                </div>
             </div>
-
         </div>
         <?php
         require_once("./includes/footer.php");
